811fd7114a5baeed938665ef13ec9d39fa1f0fd8
ryuji <fukudaryuji@fr-pc.local>
2017-09-09 15:50:09 +0800

    sumbit

---
+++ sampleTabbar/sampleTabbar/FirstViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FirstViewController.swift
+//  sampleTabbar
+//
+//  Created by 福田隆史 on 2017/09/08.
+//  Copyright © 2017年 f.R. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var indexNum: UILabel!
+    
+    //画面が表示されるたび毎回発動
+    override func viewWillAppear(_ animated: Bool) {
+        
+        //appdelegateにアクセス
+        let myApp = UIApplication.shared.delegate as! AppDelegate
+        myApp.myCount += 1
+        indexNum.text = "\(myApp.myCount)"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
